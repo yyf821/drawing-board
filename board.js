97f4b8bb32129ee7af1c50a6998368bd52b7b2d8
yyf <zlsbd01@126.com>
2023-03-18 16:32:18 +0800

    style: 添加简单样式

---
+++ board.js
@@ -0,0 +1,161 @@
+// 定义画板类
+class DrawingBoard {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.context = context;
+        this.mouseCoord = document.getElementById("mouse-coords");
+
+        // 为Canvas绑定事件监听器
+        canvas.addEventListener("mousedown", this.onMouseDown);
+        canvas.addEventListener("mousemove", this.onMouseMove);
+        canvas.addEventListener("mousemove", this.onMouseMoveDisplayCoords);
+        canvas.addEventListener("mouseup", this.onMouseUp);
+        canvas.addEventListener("mouseleave", this.clearDisplayCoords);
+
+        // 定义默认画笔样式
+        this.lineColor = "black";
+        this.lineWidth = 5;
+        this.context.lineCap = "round";
+        this.context.globalCompositeOperation = "source-over";
+
+        // 初始化变量
+        this.isDrawing = false;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.imgData = null;
+
+        this.undoStack = [];
+        this.redoStack = [];
+
+        this.currentTool = "pen";
+        this.shapeStartX = 0;
+        this.shapeStartY = 0;
+    }
+
+    onMouseDown = (event) => {
+        this.isDrawing = true;
+        this.context.strokeStyle = this.lineColor;
+        this.context.lineWidth = this.lineWidth;
+        this.lastX = event.offsetX;
+        this.lastY = event.offsetY;
+        const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        this.undoStack.push(imageData);
+        this.redoStack = [];
+
+        if (["rect", "circle"].includes(this.currentTool)) {
+            this.shapeStartX = event.offsetX;
+            this.shapeStartY = event.offsetY;
+            this.imgData = imageData;
+        }
+    };
+
+    onMouseMove = (event) => {
+        if (!this.isDrawing) return;
+
+        if (["rect", "circle"].includes(this.currentTool)) {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.context.putImageData(this.imgData, 0, 0);
+            const deltaX = event.offsetX - this.shapeStartX;
+            const deltaY = event.offsetY - this.shapeStartY;
+            this.drawShape(this.shapeStartX, this.shapeStartY, deltaX, deltaY);
+        } else {
+            this.context.beginPath();
+            this.context.moveTo(this.lastX, this.lastY);
+            this.context.lineTo(event.offsetX, event.offsetY);
+            this.context.stroke();
+
+            this.lastX = event.offsetX;
+            this.lastY = event.offsetY;
+        }
+    };
+
+    onMouseMoveDisplayCoords = (event) => {
+        const x = event.clientX - this.canvas.offsetLeft;
+        const y = event.clientY - this.canvas.offsetTop;
+        this.mouseCoord.innerHTML = `x: ${x}, y: ${y}`;
+    };
+
+    clearDisplayCoords = (event) => {
+        this.mouseCoord.innerHTML = "";
+    };
+
+    onMouseUp = (event) => {
+        this.isDrawing = false;
+        if (["rect", "circle"].includes(this.currentTool)) {
+            const deltaX = event.offsetX - this.shapeStartX;
+            const deltaY = event.offsetY - this.shapeStartY;
+
+            this.drawShape(this.shapeStartX, this.shapeStartY, deltaX, deltaY);
+        }
+    };
+
+    drawShape(startX, startY, deltaX, deltaY) {
+        switch (this.currentTool) {
+            case "circle":
+                const radius = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+                this.drawCircle(startX, startY, radius);
+                break;
+            case "rect":
+                this.drawRect(startX, startY, deltaX, deltaY);
+                break;
+            default:
+                break;
+        }
+    }
+
+    drawCircle(x, y, radius) {
+        this.context.beginPath();
+        this.context.arc(x, y, radius, 0, 2 * Math.PI);
+        this.context.stroke();
+    }
+
+    drawRect(x, y, width, height) {
+        this.context.strokeRect(x, y, width, height);
+    }
+
+    setTool(shape) {
+        this.currentTool = shape;
+        if (this.currentTool === "eraser") {
+            this.context.globalCompositeOperation = "destination-out";
+        } else {
+            this.context.globalCompositeOperation = "source-over";
+        }
+    }
+
+    setLineColor(color) {
+        this.lineColor = color;
+    }
+
+    setLineWidth(width) {
+        this.lineWidth = width;
+    }
+
+    setLineCap(style) {
+        this.context.lineCap = style;
+    }
+
+    undo() {
+        if (this.undoStack.length === 0) return;
+        this.redoStack.push(this.context.getImageData(0, 0, this.canvas.width, this.canvas.height));
+        this.context.putImageData(this.undoStack.pop(), 0, 0);
+    }
+
+    redo() {
+        if (this.redoStack.length === 0) return;
+        this.undoStack.push(this.context.getImageData(0, 0, this.canvas.width, this.canvas.height));
+        this.context.putImageData(this.redoStack.pop(), 0, 0);
+    }
+
+    saveAsImage() {
+        const link = document.createElement("a");
+        link.download = "paint.png";
+        link.href = this.canvas.toDataURL();
+        link.click();
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.undoStack = [];
+        this.redoStack = [];
+    }
+}
---
+++ main.js
@@ -0,0 +1,59 @@
+const __main = function () {
+    // 创建Canvas画布元素
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    // 创建画板对象
+    const drawingBoard = new DrawingBoard(canvas, context);
+
+    const eraserButton = document.getElementById("eraser-button");
+    eraserButton.addEventListener("click", () => {
+        drawingBoard.setTool("eraser");
+    });
+
+    const rectButton = document.getElementById("rect-button");
+    rectButton.addEventListener("click", () => {
+        drawingBoard.setTool("rect");
+    });
+
+    const circleButton = document.getElementById("circle-button");
+    circleButton.addEventListener("click", () => {
+        drawingBoard.setTool("circle");
+    });
+
+    const penButton = document.getElementById("pen-button");
+    penButton.addEventListener("click", () => {
+        drawingBoard.setTool("pen");
+    });
+
+    const undoButton = document.getElementById("undo-button");
+    undoButton.addEventListener("click", () => {
+        drawingBoard.undo();
+    });
+
+    const redoButton = document.getElementById("redo-button");
+    redoButton.addEventListener("click", () => {
+        drawingBoard.redo();
+    });
+
+    const clearButton = document.getElementById("clear-button");
+    clearButton.addEventListener("click", () => {
+        drawingBoard.clear();
+    });
+
+    const downloadButton = document.getElementById("download-button");
+    downloadButton.addEventListener("click", () => {
+        drawingBoard.saveAsImage();
+    });
+
+    const color = document.getElementById("color");
+    color.addEventListener("change", (event) => {
+        drawingBoard.setLineColor(event.target.value);
+    });
+
+    const lineWidth = document.getElementById("line-width");
+    lineWidth.addEventListener("change", (event) => {
+        drawingBoard.setLineWidth(event.target.value);
+    });
+};
+__main();
